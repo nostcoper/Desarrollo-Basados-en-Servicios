cb68f684a9f7b847ee19eb8d1c735943ab254ac6
nostcoper <carlosserrato200475@gmail.com>
2024-08-15 20:54:13 -0500

    Proyecto Uno Soap

---
+++ ProyectoUnoSoap/src/main/java/com/co/Serrato/ProyectoUnoSoap/ProyectoUnoSoapApplication.java
@@ -0,0 +1,13 @@
+package com.co.Serrato.ProyectoUnoSoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUnoSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoUnoSoapApplication.class, args);
+	}
+
+}
---
+++ ProyectoUnoSoap/src/test/java/com/co/Serrato/ProyectoUnoSoap/ProyectoUnoSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.Serrato.ProyectoUnoSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUnoSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
